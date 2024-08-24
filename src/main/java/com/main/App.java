b26e60e55c7bcaed1e2babfb0337c9fd75a3b30f
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2024-08-24 20:55:39 +0530

    adding buttons

--- src/main/java/com/main/App.java
+++ src/main/java/com/main/App.java
@@ -7,28 +7,24 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/calculator.fxml"));
+            Scene scene = new Scene(root);
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Calculator");
+            primaryStage.setResizable(false);
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
